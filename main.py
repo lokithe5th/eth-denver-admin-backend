3d409fa8db42704f8bd72c004b081cf48bf18679
lokithe5th <lourenslinde@gmail.com>
2023-02-15 11:52:38 +0200

    Tinkering

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+#from zksync2.signer.eth_signer import PrivateKeyEthSigner
+#from eth_account import Account
 import constants
-#from web3 import Web3, HTTPProvider
 from zksync2.module.module_builder import ZkSyncBuilder
-import asyncio
+import time
 
-# instantiate a web3 remote provider
-w3 = ZkSyncBuilder.build("https://starknet-goerli2.infura.io/v3/915563693acc45ca8fb73b9d82a4535c")
+filter_params = {
+    "fromBlock": 200000,
+    "toBlock": 230000,
+    "address": constants.TOKEN_ADDRESS,
+    "topics": [constants.TOPIC_TRANSFER]
+}
 
-def getChain():
-    chainId = w3.zksync.chain_id
-    print(chainId)
 
-getChain()
+#account = Account.from_key("PRIVATE_KEY")
+zksync_web3 = ZkSyncBuilder.build("https://zksync2-testnet.zksync.dev")
+
+chain_id = zksync_web3.zksync.filter(filter_params)
+#signer = PrivateKeyEthSigner(account, chain_id)
+
+time.sleep(2)
+
+logs = chain_id.get_all_entries()
+
+print(logs)
+
+for log in logs:
+    print(log['AttributeDict'])
